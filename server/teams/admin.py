e81d3b0f2b17970d3996330038db2c9069261a61
Abhijit Patil <abhijitchandrakant.p23@iiits.in>
2025-01-03 17:25:13 +0530

    change

--- server/teams/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import TechTeam, ManagementTeam, MarketingTeam, Head , Professors
-
-admin.site.register(TechTeam)
-admin.site.register(ManagementTeam)
-admin.site.register(MarketingTeam)
-admin.site.register(Head)
-admin.site.register(Professors)
